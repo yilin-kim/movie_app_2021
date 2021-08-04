1703f698d80ee1da0f2b65e864456297277c1169
yilin-kim <irenek91@korea.ac.kr>
2021-08-05 05:09:41 +0900

    3.1 State

--- src/App.js
+++ src/App.js
@@ -4,17 +4,18 @@ import PropTypes from "prop-types";
 //class component: has to extend from react component and use render method to show up on screen
 //state: object. dynamic data of component
 //this.add()가 아닌 이유: ()는 즉시 발동, add만 해야 click할 때만
+
+//setState(): refresh state and rerender
 class App extends React.Component{
   state = {
     count: 0
   };
   add = () => {
-    console.log("add");
+    this.setState(current => ({count: current.count +1 }));
   };
   minus = () => {
-    console.log("minus");
+    this.setState(current => ({count: current.count -1 }));
   };
-
 
   render(){
     return (
