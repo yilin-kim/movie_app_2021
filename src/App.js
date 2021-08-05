f47c90b87bb2677cceab206d8467a68ffed94a21
yilin-kim <irenek91@korea.ac.kr>
2021-08-05 13:12:09 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import PropTypes from "prop-types";
 //this.add()가 아닌 이유: ()는 즉시 발동, add만 해야 click할 때만
 
 //setState(): refresh state and rerender
+//Mounting
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    console.log("mount");
+  }
   state = {
     count: 0
   };
@@ -17,7 +22,14 @@ class App extends React.Component{
     this.setState(current => ({count: current.count -1 }));
   };
 
+  componentDidMount(){
+    console.log("component rendered");
+  }
+  componentDidUpdate(){
+    console.log("I just updated");
+  }
   render(){
+    console.log("I am rendering");
     return (
       <div>
         <h1>The number is: {this.state.count}</h1>
